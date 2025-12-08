26666f6982fecbf20bc6f72e8405ce06a78369fe
Justin Braun <justinbraun335@gmail.com>
2025-12-08 02:26:23 -0500

    chore: add terminal logging back to scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -11,6 +11,14 @@ from poller import (
     parse_riven_market_rivens,
 )
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+    ],
+)
+
 
 def get_total_count(url, params, headers):
     """Extract total riven and page count."""
